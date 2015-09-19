7933799cdf64618b82f268f5ac0bfd5159fec478
1gn1t0r <stefanjacholke@gmail.com>
2015-09-19 17:52:05 +0200

    Basic residence page and street view

--- TestSite/Controllers/HomeController.cs
+++ TestSite/Controllers/HomeController.cs
@@ -49,16 +49,61 @@ namespace TestSite.Controllers
 
             while(reader.Read())
             {
+                int id = reader.GetInt32(0);
                 string name = reader.GetString(1);
                 string location = reader.GetString(2);
                 int no_bathrooms = reader.GetInt32(3);
                 int no_bedrooms = reader.GetInt32(4);
                 int area = reader.GetInt32(5);
 
-                builder.AddRow(name, location, no_bathrooms.ToString(), no_bedrooms.ToString(), area.ToString());
+                string namehtml = string.Format("<a href=\"{0}\">{1}</a>", Url.Action("Residence", "Home") + "?id=" + id, name);
+
+                builder.AddRow(namehtml, location, no_bathrooms.ToString(), no_bedrooms.ToString(), area.ToString());
             }
 
             ViewBag.Message = builder.ToHTML();          
+
+            return View();
+        }
+
+        public ActionResult Residence()
+        {
+            string id = Request.QueryString["id"];
+            if (id != null)
+            {
+
+            }
+            else
+                return View();
+
+
+            MySqlConnection connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySQLConnStr"].ConnectionString);
+            connection.Open();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "Select name,street_address,no_bathrooms,no_bedrooms,area,city,province,postal_code,description,lattitude,longitude from EstatesList where estate_id=" + id;
+
+            MySqlDataReader reader = command.ExecuteReader();
+
+            reader.Read();
+            
+            ViewBag.name = reader.GetString(0);
+            ViewBag.street_address = reader.GetString(1);
+            ViewBag.no_bathrooms = reader.GetInt32(2);
+            ViewBag.no_bedrooms = reader.GetInt32(3);
+            ViewBag.area = reader.GetInt32(4);
+            ViewBag.city = reader.GetString(5);
+            ViewBag.province = reader.GetString(6);
+            ViewBag.postal_code = reader.GetString(7);
+            ViewBag.description = reader.GetString(8);
+            ViewBag.lattitude = reader.GetFloat(9);
+            ViewBag.longitude = reader.GetFloat(10);
+
+            ViewBag.Message = "Your contact page.";
+
+
+            ViewBag.StreetView = string.Format("https://www.google.com/maps/embed/v1/streetview?location={0}%2C{1}&key={2}", ViewBag.lattitude, ViewBag.longitude, "AIzaSyDlnvQdhrcHR6dv2UyyVmzWT_pzaPcmwTo");
+
 
             return View();
         }
