9de55087f0be9f2e19b4a7d3556939b78a3bc595
1gn1t0r <stefanjacholke@gmail.com>
2015-09-29 18:03:07 +0200

    React on search page Province and city

--- TestSite/App_Start/BundleConfig.cs
+++ TestSite/App_Start/BundleConfig.cs
@@ -24,6 +24,11 @@ namespace TestSite
                       "~/Scripts/respond.js"
                       ));
 
+            bundles.Add(new ScriptBundle("~/bundles/react").Include(
+                      "~/Scripts/react.js",
+                      "~/Scripts/JSXTransformer.js"
+                      ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
